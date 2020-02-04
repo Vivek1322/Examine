ccd9bd1b0a8616a1b8732a7c4899642ae534400b
vivek1322 <vivekjangra707@gmail.com>
2020-02-05 02:55:05 +0530

    razorpay implementation

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 var express         = require("express"),
     app             = express(),
     bodyParser      = require("body-parser"),
-    methodOverride  = require("method-override");
+    methodOverride  = require("method-override"),
+    razorpay		= require("razorpay"),
+    stripe			= require("stripe")("sk_test_PdVAcrsDy9xwLt12PhlBEnml00B8oaAVc2");
 
 
 
@@ -16,6 +18,79 @@ app.use(express.static("public"));
 app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({extended:true}));
 
+
+//ROUTES
+// app.get("/", function(req, res){
+// 	res.render("home");
+// });
+
+// app.post("/checkout", function(req, res){
+// 	(async () => {
+// 		const charge = await stripe.charges.create({
+// 	    	amount: 69900,
+// 	    	currency: 'inr',
+// 	    	source: req.body.stripeToken,
+// 	  	});
+// 	})();
+// 	res.send(req.body);
+// });
+
+var instance = new razorpay({
+	key_id: 'rzp_test_6gkeHKqbrpJ1OY',
+	key_secret: 'JHJ3AQbLb9ognTbMSbv2i6oz'
+});
+
+app.get('/', function(req, res){
+	res.render('razorpay');
+});
+
+app.get('/pay', function(req, res){
+	var options = {
+		amount: 5000,
+		currency: "INR"
+        };
+	instance.orders.create(options, function(err, order){
+		if(err){
+			res.send(err);
+        }
+        else{
+            console.log("**********Order Created***********");
+            console.log(order);
+            console.log("**********Order Created***********");
+            res.render('pay', {orderId: order.id});
+        }
+	});
+});
+
+
+app.post('/purchase', (req,res) =>{
+    payment_id =  req.body;
+    console.log("**********Payment authorized***********");
+    console.log(payment_id);
+    console.log("**********Payment authorized***********");
+    instance.payments.fetch(payment_id.razorpay_payment_id).then((response) => {
+    console.log("**********Payment instance***********");
+    console.log(response); 
+    console.log("**********Payment instance***********")
+    instance.payments.capture(payment_id.razorpay_payment_id, response.amount).then((response) => {
+    console.log(response);
+    res.redirect(`/success/?paymentId=${response.id}`);
+}).catch((error) => {
+  console.log(error);
+});
+}).catch((error) => {
+  console.log(error);
+});
+});
+
+app.get('/success', function(req, res){
+    res.send(req.query.paymentId);
+});
+
+app.post('/payment',(req,res)=>{
+    console.log('inside callback url')
+    return res.send('200',req.body);
+});
 
 
 //LISTEN ON PORT
